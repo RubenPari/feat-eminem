c7c8251b1b452563ef14fb5c63e937d9ba87bb92
RubenPari <rubenpari.developer@gmail.com>
2022-05-06 18:14:35 +0200

    developing Login endpoint

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -5,3 +5,9 @@ import "net/http"
 func Index(w http.ResponseWriter, _ *http.Request) {
 	_, _ = w.Write([]byte("Hello World"))
 }
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	// call the endpoint for login the user
+	endpoint := "https://api.spotify.com/v1/me"
+	_, _ = http.Get(endpoint)
+}
