e3b10ef1c32e0da7a9f9d41e61fead117100e0b8
RubenPari <rubenpari.developer@gmail.com>
2022-05-07 14:44:53 +0200

    implemented AuthorizeAccount controller

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,13 +1,36 @@
 package controllers
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"os"
+)
 
-func Index(w http.ResponseWriter, _ *http.Request) {
-	_, _ = w.Write([]byte("Hello World"))
+// AuthorizeAccount
+// this endpoint redirect to the Spotify login page
+// for login and get access token for future endpoint.
+func AuthorizeAccount(w http.ResponseWriter, r *http.Request) {
+	endpoint := "https://accounts.spotify.com/authorize"
+	respAuth, errAuth := http.Get(endpoint + "?response_type=code" +
+		"&client_id=" + os.Getenv("CLIENT_ID") +
+		"&scope=user-read-private user-read-email" +
+		"&redirect_uri=http://localhost:4000" +
+		"&state=this is a random string")
+
+	if errAuth != nil {
+		log.Fatalln("Error to call endpoint:", endpoint, "\n", errAuth)
+	}
+
+	if respAuth.StatusCode == http.StatusOK {
+		log.Default().Println("Redirect to GetAccessToken endpoint")
+		log.Default().Println("Response information", &r)
+		http.Redirect(w, r, "/access-token", http.StatusFound)
+	}
 }
 
-func Login(w http.ResponseWriter, r *http.Request) {
-	// call the endpoint for login the user
-	endpoint := "https://api.spotify.com/v1/me"
-	_, _ = http.Get(endpoint)
+// GetAccessToken
+// this endpoint get the access token from the
+// AuthorizeAccount endpoint
+func GetAccessToken(w http.ResponseWriter, r *http.Request) {
+	_, _ = w.Write([]byte("GetAccessToken endpoint"))
 }
