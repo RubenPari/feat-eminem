69c68c3e23e266cf65f38d402b6837b98e1ab2e7
RubenPari <rubenpari.developer@gmail.com>
2023-04-21 20:28:12 +0200

    fix track model

--- Models/Track.cs
+++ Models/Track.cs
@@ -4,7 +4,7 @@ public class Track
 {
     [Key]
     [Required]
-    public int Id { get; set; }
+    public string Id { get; set; }
     [Required]
     public string Name { get; set; }
     [Required]
