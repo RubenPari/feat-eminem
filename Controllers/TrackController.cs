28a9394359d0cd8efbac4ef1854c3d893f4f093f
RubenPari <rubenpari.developer@gmail.com>
2023-04-22 14:11:16 +0200

    mapped crud track endpoints

--- Controllers/TrackController.cs
+++ Controllers/TrackController.cs
@@ -1,55 +1,107 @@
-using feat_eminem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace feat_eminem.Controllers;
+
+using Models;
 using Microsoft.AspNetCore.Mvc;
 using SpotifyAPI.Web;
 
-namespace feat_eminem.Controllers
+[ApiController]
+[Route("[controller]")]
+public class TrackController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class TrackController : ControllerBase
+    private readonly DatabaseContext _db;
+
+    public TrackController(DatabaseContext db)
     {
-        private readonly DatabaseContext _db;
+        _db = db;
+    }
 
-        public TrackController(DatabaseContext db)
+    [HttpPost("add/{id}")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> AddAsync(string id)
+    {
+        // TODO: mapped middleware for check if the user is logged in
+
+        var spotify = new SpotifyClient(
+            HttpContext.Session.GetString("AccessToken")!);
+
+        var track = await spotify.Tracks.Get(id);
+
+        // check if url parameter is valid
+        // for make sure that the track is
+        // valid
+        if (!track.Uri.Contains("https://open.spotify.com/track/"))
         {
-            _db = db;
+            return BadRequest("Track with given id not found");
         }
 
-        [HttpPost("add/{id}")]
-        [ProducesResponseType(StatusCodes.Status201Created)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> AddAsync(string id)
+        // save track to db
+        await _db.Tracks!.AddAsync(new Track
         {
-            // TODO: mapped middleware for check if the user is logged in
+            Id = track.Id,
+            Name = track.Name,
+            Artist = track.Artists[0].Name,
+            Album = track.Album.Name,
+            SpotifyUrl = track.ExternalUrls["spotify"]
+        });
 
-            var spotify = new SpotifyClient(
-                HttpContext.Session.GetString("AccessToken")!);
+        var saved = await _db.SaveChangesAsync();
 
-            var track = await spotify.Tracks.Get(id);
-
-            if (track == null)
-            {
-                return NotFound("Track with given id not found");
-            }
-
-            // save track to db
-            await _db.Tracks!.AddAsync(new Track
-            {
-                Id = track.Id,
-                Name = track.Name,
-                Artist = track.Artists[0].Name,
-                Album = track.Album.Name,
-                SpotifyUrl = track.ExternalUrls["spotify"]
-            });
-
-            var saved = await _db.SaveChangesAsync();
-
-            if (saved == 0)
-            {
-                return BadRequest("Track could not be saved");
-            }
-
-            return Created("Track added successfully", track);
+        if (saved == 0)
+        {
+            return BadRequest("Track could not be saved");
         }
+
+        return Created("Track added successfully", track);
+    }
+
+    [HttpGet("get/{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetAsync(string id)
+    {
+        var track = await _db.Tracks!.FindAsync(id);
+
+        if (track == null)
+        {
+            return NotFound("Track with given id not found");
+        }
+
+        return Ok(track);
+    }
+
+    [HttpGet("get-all")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAllAsync()
+    {
+        var tracks = await _db.Tracks!.ToArrayAsync();
+
+        return Ok(tracks);
+    }
+
+    [HttpDelete("delete/{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> DeleteAsync(string id)
+    {
+        var track = await _db.Tracks!.FindAsync(id);
+
+        if (track == null)
+        {
+            return NotFound("Track with given id not found");
+        }
+
+        _db.Tracks!.Remove(track);
+
+        var deleted = await _db.SaveChangesAsync();
+
+        if (deleted == 0)
+        {
+            return BadRequest("Track could not be deleted");
+        }
+
+        return Ok("Track deleted successfully");
     }
 }
