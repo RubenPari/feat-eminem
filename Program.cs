30f6fadefca4e52018793ef5e0d47f8712faee32
RubenPari <rubenpari.developer@gmail.com>
2023-04-25 15:44:36 +0200

    added serilog to project

--- Program.cs
+++ Program.cs
@@ -1,14 +1,18 @@
 using feat_eminem.Middlewares;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
 builder.Services.AddSession(options =>
 {
     options.IdleTimeout = TimeSpan.FromMinutes(30);
     options.Cookie.HttpOnly = true;
     options.Cookie.IsEssential = true;
 });
+
+builder.Host.UseSerilog((context, configuration) => { configuration.ReadFrom.Configuration(context.Configuration); });
 
 var app = builder.Build();
 
@@ -17,6 +21,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.UseCheckAuthMiddleware();
+
+app.UseSerilogRequestLogging();
 
 app.MapControllers();
 
