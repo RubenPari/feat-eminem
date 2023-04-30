92f257e09218134318e13da472f421745269ff41
RubenPari <rubenpari.developer@gmail.com>
2023-04-30 23:25:31 +0200

    reimplemented DatabaseContext.cs

--- DatabaseContext.cs
+++ DatabaseContext.cs
@@ -1,21 +1,18 @@
 using feat_eminem.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace feat_eminem
+namespace feat_eminem;
+
+public class DatabaseContext : DbContext
 {
-    public class DatabaseContext : DbContext
+    public DbSet<Track>? Tracks { get; set; }
+
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
     {
-        private readonly IConfiguration _config;
-        public DbSet<Track>? Tracks { get; set; }
+    }
 
-        public DatabaseContext(DbContextOptions<DatabaseContext> options, IConfiguration config) : base(options)
-        {
-            _config = config;
-        }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseNpgsql(_config["ConnectionString"]);
-        }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySQL(Environment.GetEnvironmentVariable("MYSQL_CONNECTION_STRING")!);
     }
 }
